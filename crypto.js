b6b00b5b635d8f5750df7126f07f36c7d519f94f
Mesut <yildirimmesutyigit@gmail.com>
2023-06-19 23:24:48 +0300

    new commit

--- crypto.js
+++ crypto.js
@@ -33,36 +33,65 @@ const getCoinDataFromApi = async () => {
   //   const response = await fetch(url, options)
   //   .then((response) => response.json())
   //   .then((result) => console.log(result.data.coins[0]));
-  const response = await axios(url, options);
-  //console.log(response.data.data.coins[0]);
-  //obj. dest.
-  const { price, name, change, iconUrl, symbol } = response.data.data.coins[0];
-  //console.log(iconUrl);
+  try {
+    const response = await axios(url, options);
+    //console.log(response.data.data.coins[0]);
+    //obj. dest.
+    const { price, name, change, iconUrl, symbol } =
+      response.data.data.coins[0];
+    //console.log(iconUrl);
 
-  const createdLi = document.createElement("li");
-  createdLi.classList.add("coin");
-  createdLi.innerHTML = `
-        <h2 class="coin-name" data-name=${name}>
-            <span>${name}</span>
-            <sup>${symbol}</sup>
-        </h2>
-        <div class="coin-temp">$${Number(price).toFixed(6)}</div>
-        <figure>
-            <img class="coin-icon" src=${iconUrl}>                
-            <figcaption style='color:${change < 0 ? "red" : "green"}'>
-                <span><i class="fa-solid fa-chart-line"></i></span>
-                <span>${change}%</span>
-            </figcaption>
-        </figure>
-        <span class="remove-icon">
-            <i class="fas fa-window-close" style="color:red"></i>
-        </span>`;
-  //append vs. prepend
-  //append vs appendChild
-  coinList.prepend(createdLi);
-  //remove func.
-  createdLi.querySelector(".remove-icon").addEventListener("click", () => {
-    createdLi.remove();
-  });
-  //
+    //Coin Control!!!
+    const coinNameSpans = coinList.querySelectorAll("h2 span");
+    //filter vs. map(array)
+    if (coinNameSpans.length > 0) {
+      const filteredArray = [...coinNameSpans].filter(
+        (span) => span.innerText == name
+      );
+      console.log(filteredArray);
+      if (filteredArray.length > 0) {
+        msgSpan.innerText = `You already know the data for ${name}, Please search for another coin 😉`;
+        setTimeout(() => {
+          msgSpan.innerText = "";
+        }, 3000);
+        return;
+      }
+    }
+
+    //continue xxxx return
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("coin");
+    createdLi.innerHTML = `
+            <h2 class="coin-name" data-name=${name}>
+                <span>${name}</span>
+                <sup>${symbol}</sup>
+            </h2>
+            <div class="coin-temp">$${Number(price).toFixed(6)}</div>
+            <figure>
+                <img class="coin-icon" src=${iconUrl}>                
+                <figcaption style='color:${change < 0 ? "red" : "green"}'>
+                    <span><i class="fa-solid fa-chart-line"></i></span>
+                    <span>${change}%</span>
+                </figcaption>
+            </figure>
+            <span class="remove-icon">
+                <i class="fas fa-window-close" style="color:red"></i>
+            </span>`;
+    //append vs. prepend
+    //append vs appendChild
+    coinList.prepend(createdLi);
+    //remove func.
+    createdLi.querySelector(".remove-icon").addEventListener("click", () => {
+      createdLi.remove();
+    });
+  } catch (error) {
+    //error logging
+    //postErrorLog("crypto.js", "getCoinDataFromApi", new Date(), error...);
+    msgSpan.innerText = `Coin not found!`;
+    setTimeout(() => {
+      msgSpan.innerText = "";
+    }, 3000);
+  }
 };
+//http response codes
+//rest api vs. soap api
