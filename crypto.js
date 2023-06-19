98569bd8bb644573559ac6e008bfc4ffe9252898
Mesut <yildirimmesutyigit@gmail.com>
2023-06-19 20:26:29 +0300

    new commit

--- crypto.js
+++ crypto.js
@@ -1,10 +1,11 @@
 const form = document.querySelector(".top-bannerform");
 const input = document.querySelector(".top-bannerinput");
 // .class1.calss2 vs. .class1 .class2
-const msSpan = document.querySelector(".container .msg");
+const msgSpan = document.querySelector(".container .msg");
 const coinList = document.querySelector(".ajax-section .container .coins");
 
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
   getCoinDataFromApi();
 });
 
